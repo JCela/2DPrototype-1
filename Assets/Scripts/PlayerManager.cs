9daf438e8e51de3871daf167f9cb501d54527244
Jackson Cella <cjc771@nyu.edu>
2019-02-06 13:23:06 -0500

    Initial Commit.

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+    
+	private Vector3 moveDirection = Vector3.zero;
+    private CharacterController cc;
+	public SpriteRenderer playerSprite;
+	//private Rigidbody2D rb;
+
+	public Transform startDash;
+	public Transform endDash;
+	
+	private float speed = 10f;
+	private float jumpSpeed = 8.0f;
+	private float gravity = 20.0f;
+	private float dashLength;
+	private float dashStartTime;
+	private bool isGrounded = false;
+
+	private float dashTimer = 0f;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+		//rb = GetComponent<Rigidbody2D>();
+		dashStartTime = Time.time;
+		dashStartTime  = Vector3.Distance(startDash.position, endDash.position);
+	}
+	
+	void Update()
+	{
+		dashTimer -= 1f;
+		if (this.tag == "Player1") {
+			
+			float translationP1 = Input.GetAxis("JoyP1") * speed;
+			moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+			this.cc.Move(moveDirection * Time.deltaTime);
+			
+			
+			if (translationP1 < 0)
+			{
+				StartCoroutine(PlayerMoveLeftCoroutine());
+
+			}
+			if (translationP1 > 0)
+			{
+				StartCoroutine(PlayerMoveRightCoroutine());
+			}
+			if (cc.isGrounded)
+			{
+				if (Input.GetButton("JumpP1"))
+				{
+					this.moveDirection.y = jumpSpeed;
+				}
+				else
+				{
+					this.moveDirection.y = 0;
+				}
+
+				if (Input.GetButton("DashP1") && dashTimer < 0f)
+				{
+					StartCoroutine(PlayerDashCoroutine());
+				}
+				
+			}
+
+		}
+		if (this.tag == "Player2")
+		{
+			
+			float translationP2 = Input.GetAxis("JoyP2") * speed;
+			moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+			this.cc.Move(moveDirection * Time.deltaTime);
+			
+			if (translationP2 < 0)
+			{
+				StartCoroutine(PlayerMoveLeftCoroutine());
+
+			}
+			if (translationP2 > 0)
+			{
+				StartCoroutine(PlayerMoveRightCoroutine());
+			}
+			if (cc.isGrounded)
+			{
+				if (Input.GetButton("JumpP2"))
+				{
+					this.moveDirection.y = jumpSpeed;
+				}
+				else
+				{
+					this.moveDirection.y = 0;
+				}
+				
+			}
+			if (Input.GetButton("DashP2"))
+			{
+				StartCoroutine(PlayerDashCoroutine());
+			}
+		}
+	}
+	IEnumerator PlayerDashCoroutine()
+	{
+		float distCovered = (dashStartTime * speed+ 20);
+
+		// Fraction of journey completed = current distance divided by total distance.
+		float fracJourney = distCovered / dashLength;
+
+		// Set our position as a fraction of the distance between the markers.
+		transform.position = Vector3.Lerp(startDash.position, endDash.position, fracJourney);
+		dashTimer = 200f;
+		yield return 60;
+		yield break;
+		;
+	}
+	IEnumerator PlayerMoveLeftCoroutine()
+	{
+		
+		//this.cc.Move(transform.right * -speed *Time.deltaTime);
+		this.playerSprite.flipX = true;
+		yield break;
+	}
+	IEnumerator PlayerMoveRightCoroutine()
+	{
+		
+		//this.cc.Move(transform.right * speed *Time.deltaTime);
+		this.playerSprite.flipX = false;
+		yield break;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	private SpriteRenderer sr;
+	private float speed = 50f;
+	private bool isGrounded;
+	private float jumpBuffer = 10;
+
+	void Start ()
+	{
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+	{
+		jumpBuffer -= 1;
+		Ray groundCheckRay = new Ray(this.transform.position, this.transform.up*-1);
+		float groundCheckRayLength = 0.43f;
+		Debug.DrawRay(groundCheckRay.origin, groundCheckRay.direction * groundCheckRayLength, Color.yellow);
+		if (Physics.Raycast(groundCheckRay, groundCheckRayLength))
+		{
+			isGrounded = true;
+		}
+		if (this.tag == "Player1")
+		{
+			
+			float translationP1 = Input.GetAxis("JoyP1") * speed;
+			if (translationP1 < 0)
+			{
+				StartCoroutine(PlayerMoveLeftCoroutine());
+
+			}
+			if (translationP1 > 0)
+			{
+				StartCoroutine(PlayerMoveRightCoroutine());
+			}
+
+			if (isGrounded == true && jumpBuffer < 0)
+			{
+				if (Input.GetButton("JumpP1"))
+				{
+					StartCoroutine(PlayerMoveUpCoroutine());
+					isGrounded = false;
+					jumpBuffer = 10;
+				}
+			}
+
+		}
+
+		if (this.tag == "Player2")
+		{
+			float translationP2 = Input.GetAxis("JoyP2") * speed;
+			if (translationP2 < 0)
+			{
+				StartCoroutine(PlayerMoveLeftCoroutine());
+
+			}
+			if (translationP2 > 0)
+			{
+				StartCoroutine(PlayerMoveRightCoroutine());
+			}
+			if (isGrounded == true && jumpBuffer < 0)
+			{
+				if (Input.GetButton("JumpP2"))
+				{
+					StartCoroutine(PlayerMoveUpCoroutine());
+					isGrounded = false;
+					jumpBuffer = 10;
+				}
+			}
+		}
+	}
+
+	IEnumerator PlayerMoveLeftCoroutine()
+	{
+		rb.AddForce(transform.right * -speed);
+		yield break;
+	}
+	IEnumerator PlayerMoveRightCoroutine()
+	{
+		rb.AddForce(transform.right* speed);
+		yield break;
+	}
+	IEnumerator PlayerMoveUpCoroutine()
+	{
+		rb.AddForce(transform.up* speed*20);
+		yield break;
+	}
+	
+}
