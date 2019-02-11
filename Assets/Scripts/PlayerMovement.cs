d72e8be2f53a57710e506731abf016363aca58ad
Jackson Cella <cjc771@nyu.edu>
2019-02-10 20:59:56 -0500

    Movement mk1 - Added Material attributes to physical movement - ice slippery, steel/copper weight change, wood default for now.  Added reference to Andrews script just commented out for now.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,10 @@ public class PlayerMovement : MonoBehaviour
 	private bool isGrounded;
 	private float jumpBuffer = 10;
 	public LayerMask ground_layers;
-	public Transform top_left_gc;
+	public Transform top_left_gc; //this is badly named sorry guys, it is the ground check that needs to be assigned in the inspector
 	private float gcRadius = 0.2f;
+
+	private static int currentMat;
 	
 	void Start ()
 	{
@@ -21,23 +23,43 @@ public class PlayerMovement : MonoBehaviour
 
 	void Update ()
 	{
+		//currentMat = MaterialGrabber.material;  Once integration is complete.
+		currentMat = 1;
 		jumpBuffer -= 1;
 		isGrounded = Physics2D.OverlapCircle(top_left_gc.position, gcRadius , ground_layers);
 	
 		if (this.tag == "Player1")
 		{
-			
+			if (currentMat == 0)
+			{
+				this.rb.gravityScale = 10;
+				Debug.Log("STEEL");
+			}
+			else if(currentMat == 1){
+				Debug.Log("ICE");
+				this.rb.gravityScale = 2;
+				//this.rb.linearDr
+				rb.drag = 0;
+			}
+			else if(currentMat == 2){
+				Debug.Log("WOOD");
+				this.rb.gravityScale = 2;
+			}
+			else if(currentMat == 3){
+				Debug.Log("COPPER");
+				this.rb.gravityScale = 7;
+			}
+				
 			float translationP1 = Input.GetAxis("JoyP1") * speed;
-			if (translationP1 < 0 || Input.GetKey(KeyCode.A))
+			if ((translationP1 < 0 || Input.GetKey(KeyCode.A)) && currentMat != 0)
 			{
 				StartCoroutine(PlayerMoveLeftCoroutine());
 
 			}
-			if (translationP1 > 0 || Input.GetKey(KeyCode.D))
+			if ((translationP1 > 0 || Input.GetKey(KeyCode.D)) && currentMat != 0)
 			{
 				StartCoroutine(PlayerMoveRightCoroutine());
 			}
-
 			if (isGrounded == true && jumpBuffer < 0)
 			{
 				if (Input.GetButton("JumpP1"))
